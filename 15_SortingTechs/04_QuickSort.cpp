bfa59f4acfa886dd0f4b541566ad4b6966bf482c
Sagar Gupta <sg85207@gmail.com>
2022-05-13 09:28:51 +0530

    13-05-2022

--- 15_SortingTechs/04_QuickSort.cpp
+++ 15_SortingTechs/04_QuickSort.cpp
@@ -8,9 +8,34 @@ void Display(int A[],int n){
     cout<<endl;
 }//O(n)
 
-void QuickSort(int A[],int n){
-    
-}//O(n*Log(n))
+int partition(int A[],int l,int h){
+    int p = A[l];
+    int i=l+1,j=h;
+    while(i<=j){
+        while(A[i]<p){
+            i++;
+            if(i>j) break;
+        }
+        while(A[j]>p){
+            j--;
+            if(i>j) break;
+        }
+        if(i>j) break;
+        swap(A[i],A[j]);
+        i++;
+        j--;
+    }
+    swap(A[l],A[j]);
+    return j;
+}
+
+void QuickSort(int A[],int l,int h){
+    if(l<h){
+        int j = partition(A,l,h);
+        QuickSort(A,l,j-1);
+        QuickSort(A,j+1,h);
+    }
+}//O(nLog(n))
 
 int main(){
     int A[]={5,8,1,7,3,6,2,9,10,4};
@@ -19,7 +44,7 @@ int main(){
     cout<<"Array Before Sort : ";
     Display(A,n);
 
-    QuickSort(A,n);
+    QuickSort(A,0,n-1);
 
     cout<<"Array After  Sort : ";
     Display(A,n);
